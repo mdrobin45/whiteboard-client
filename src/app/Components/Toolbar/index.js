fbad128c06270f80a632c10537e694974761eaeb
Md Robin Rana <developer.smrobin45@gmail.com>
2023-12-28 15:20:03 +0600

    icon added

--- src/app/Components/Toolbar/index.js
+++ src/app/Components/Toolbar/index.js
@@ -1,7 +1,22 @@
+import { FaEraser, FaPencil } from "react-icons/fa6";
+import { MdOutlineUndo, MdRedo } from "react-icons/md";
+import styles from "./styles.module.css";
+
 const Toolbar = () => {
    return (
-      <div className="w-10 h-10 rounded-full overflow-hidden border">
-         <input className="w-full h-full" type="color" />
+      <div className={styles.iconsWrapper}>
+         <div className={styles.icon}>
+            <FaPencil />
+         </div>
+         <div className={styles.icon}>
+            <FaEraser className="text-3xl" />
+         </div>
+         <div className={styles.icon}>
+            <MdRedo className="text-3xl" />
+         </div>
+         <div className={styles.icon}>
+            <MdOutlineUndo className="text-3xl" />
+         </div>
       </div>
    );
 };
