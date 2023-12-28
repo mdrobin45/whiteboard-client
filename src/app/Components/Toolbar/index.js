70b7d9c9a8068b243529fdb95c20927214068e89
Md Robin Rana <developer.smrobin45@gmail.com>
2023-12-28 20:45:07 +0600

    toolbar color palette added

--- src/app/Components/Toolbar/index.js
+++ src/app/Components/Toolbar/index.js
@@ -1,13 +1,40 @@
+"use client";
+import Sketch from "@uiw/react-color-sketch";
+import { useState } from "react";
 import { FaEraser, FaPencil } from "react-icons/fa6";
 import { MdOutlineUndo, MdRedo } from "react-icons/md";
 import styles from "./styles.module.css";
 
 const Toolbar = () => {
+   const [hex, setHex] = useState("#fff");
+   const [openPalette, setOpenPalette] = useState(false);
    return (
       <div className={styles.iconsWrapper}>
          <div className={styles.icon}>
             <FaPencil />
          </div>
+         {openPalette ? (
+            <>
+               <div className="absolute">
+                  <Sketch
+                     style={{ marginLeft: 20 }}
+                     color={hex}
+                     onChange={(color) => {
+                        setHex(color.hex);
+                     }}
+                  />
+               </div>
+            </>
+         ) : (
+            ""
+         )}
+
+         <button
+            onClick={() => {
+               setOpenPalette(!openPalette);
+            }}
+            className="w-7 h-7 bg-red-500 rounded-full border-2 border-gray-300"></button>
+
          <div className={styles.icon}>
             <FaEraser className="text-3xl" />
          </div>
